e1148d3dbb0f30e603e1377fa00b4b140120c52b
Kai Zhu <kz2217@ic.ac.uk>
2021-04-21 19:08:43 +0800

    Used compressed commands

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <Simulator :animation="animation" :environments="environments" />
+    <div v-if="error" id="error_msg">An error was met when parsing the commands,
+      please check whether the url is correct.
+    </div>
+    <Simulator v-if="!error" :animation="animation" :environments="environments"/>
   </div>
 </template>
 
@@ -8,13 +11,32 @@
 import Simulator from "./components/Simulator.vue";
 
 export default {
-  components: { Simulator },
+  components: {Simulator},
   data() {
     return {
-      animation: JSON.parse(this.$route.query.commands || "[]"),
-      environments: []
+      animation: [],
+      environments: [],
+      error: null
     };
+  },
+  mounted() {
+    const compressed_str = this.$route.query.commands;
+    if (compressed_str) {
+      const pako = require('pako');
+      const compressed = Buffer(compressed_str, "base64");
+      try {
+        this.animation = JSON.parse(pako.inflate(compressed, {to: "string"}));
+      } catch (err) {
+        this.error = err;
+      }
+    }
   }
 };
 </script>
 
+<style scoped>
+#error_msg {
+  color: red;
+  margin: 50px;
+}
+</style>
